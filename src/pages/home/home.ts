31e0996f38be5233f783f65fc468cd6d14cc1a9e
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2017-12-31 17:27:24 +0530

    initial file push

---
+++ src/pages/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public category: string = 'movies';
+
+  constructor(public navCtrl: NavController) { }
+
+}
